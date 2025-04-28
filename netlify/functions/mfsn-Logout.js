6860e09bef0aa2bceee35e3f7250d1a897592134
mjeter14 <contact@seeimprove.com>
2025-04-27 21:58:01 -0500

    Delete netlify/functions/mfsn-Logout.js

--- netlify/functions/mfsn-Logout.js
+++
@@ -1,8 +0,0 @@
-// netlify/functions/mfsn-logout.js
-const { callMFSN } = require('./_mfsn');
-
-exports.handler = async ({ body }) => {
-  const { token } = JSON.parse(body);
-  await callMFSN('/api/auth/logout', token, {});
-  return { statusCode: 200, body: 'Logged out' };
-};
