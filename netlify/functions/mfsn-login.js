82caeb7f08760d7838f5e4f1e818d9c0e697d9bf
mjeter14 <contact@seeimprove.com>
2025-05-01 12:59:52 -0500

    Update mfsn-login.js

--- netlify/functions/mfsn-login.js
+++ netlify/functions/mfsn-login.js
@@ -1,18 +1,36 @@
+// netlify/functions/mfsn-login.js
 const { callMFSN } = require('./_mfsn');
 
-exports.handler = async ({ httpMethod, body }) => {
-  if (httpMethod !== 'POST') return { statusCode: 405, body: 'Only POST' };
-  const { email, password } = JSON.parse(body);
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Only POST allowed' };
+  }
+
+  let email, password;
+  try {
+    ({ email, password } = JSON.parse(event.body));
+  } catch {
+    return { statusCode: 400, body: 'Invalid JSON' };
+  }
 
   try {
+    // this will POST form-data to /login
     const data = await callMFSN('login', null, { email, password });
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ token: data.token, userId: data.userId })
+      body: JSON.stringify({
+        token:  data.token,
+        userId: data.userId
+      })
     };
   } catch (err) {
-    return { statusCode: 401, body: JSON.stringify({ error: err.message }) };
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ error: err.message })
+    };
   }
 };
+
 
 
